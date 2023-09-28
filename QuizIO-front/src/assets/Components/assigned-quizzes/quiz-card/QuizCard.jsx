8346cf0b4f490baf1ed2f0e45fcc90c2f34e6efd
Daniil <ternak04@gmail.com>
2023-09-28 15:39:14 +0300

    refactoring

--- QuizIO-front/src/assets/Components/assigned-quizzes/quiz-card/QuizCard.jsx
+++ QuizIO-front/src/assets/Components/assigned-quizzes/quiz-card/QuizCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import styles from './QuizCard.module.css';
-import { format, parseISO } from "date-fns";
+import { format, parseISO } from 'date-fns';
 
 const QuizCard = ({ quiz }) => {
+  const { quizName, quizDescription, quizCreationDate } = quiz;
+
   const formattedQuizCreationDate = format(
-    parseISO(quiz.quizCreationDate),
-    "MM/dd/yyyy"
+    parseISO(quizCreationDate),
+    'MM/dd/yyyy'
   );
+
   return (
     <div className={styles.Card}>
-      <h2 className={styles.QuizName}>{quiz.quizName}</h2>
-      <p className={styles.QuizDesc}>{quiz.quizDescription}</p>
+      <h2 className={styles.QuizName}>{quizName}</h2>
+      <p className={styles.QuizDesc}>{quizDescription}</p>
       <p className={styles.QuizDate}>Created on: {formattedQuizCreationDate}</p>
     </div>
   );
-}
+};
 
 export default QuizCard;
--- QuizIO-front/src/assets/Services/Quiz.service.js
+++ QuizIO-front/src/assets/Services/Quiz.service.js
@@ -1,35 +1,53 @@
 import axios from "axios";
 
+const API_BASE_URL = "https://localhost:7299";
+
+const validateNonEmptyString = (value) =>
+  typeof value === "string" && value.trim() !== "";
+
 export const QuizService = {
   async getAssignedQuizzes(username) {
-    if (username !== undefined && username != null && username !== "") {
+    if (!validateNonEmptyString(username)) {
+      return [];
+    }
+
+    try {
       const response = await axios.get(
-        `https://localhost:7299/Quiz/assigned/${username}`
+        `${API_BASE_URL}/Quiz/assigned/${username}`
       );
       return response.data;
-    } else {
+    } catch (error) {
+      console.error("Error fetching assigned quizzes:", error);
       return [];
     }
   },
 
   async getQuizByID(quizId) {
-    if (quizId !== undefined && quizId != null && quizId !== "") {
-      const response = await axios.get(
-        `https://localhost:7299/Quiz/id/${quizId}`
-      );
+    if (!validateNonEmptyString(quizId)) {
+      return [];
+    }
+
+    try {
+      const response = await axios.get(`${API_BASE_URL}/Quiz/id/${quizId}`);
       return response.data;
-    } else {
+    } catch (error) {
+      console.error("Error fetching quiz by ID:", error);
       return [];
     }
   },
 
   async updateQuiz(quiz) {
-    if (quiz !== undefined && quiz != null && quiz !== "") {
-      const response = await axios.post(
-        `https://localhost:7299/Quiz/update`,
-        quiz
-      );
-      return response.data;
+    if (quiz && Object.keys(quiz).length > 0) {
+      try {
+        const response = await axios.post(
+          `${API_BASE_URL}/Quiz/update`,
+          quiz
+        );
+        return response.data;
+      } catch (error) {
+        console.error("Error updating quiz:", error);
+        return [];
+      }
     } else {
       return [];
     }
